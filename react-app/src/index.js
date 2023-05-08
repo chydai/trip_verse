2cef4abd43fee69d8ddc3b8e31c5c1c0ba9aef32
Chunyang Dai <chydai.p@gmail.com>
2023-05-07 21:31:19 -0400

    cicd test

--- react-app/src/index.js
+++ react-app/src/index.js
@@ -36,3 +36,5 @@ root.render(
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
 serviceWorker.unregister();
+
+// ==============================|| REACT DOM RENDER  ||============================== //
